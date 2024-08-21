dcdb8703f2c89356f50e50435e4329a3b577b991
BasharMOhamed <bashar.eg6645@gmail.com>
2024-08-21 17:38:38 +0300

    done

--- src/Components/home/home.component.ts
+++ src/Components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { NgFor, NgIf, TitleCasePipe } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
-import {Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, OnInit } from '@angular/core';
 import { productsService } from '../../app/services/products.service';
 import { FormsModule } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
@@ -10,39 +10,39 @@ import { BehaviorSubject } from 'rxjs';
   standalone: true,
   imports: [NgFor, NgIf, TitleCasePipe, MatIconModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   products: any[] = [];
-  currentPage:number = 1;
+  currentPage: number = 1;
   page!: BehaviorSubject<number>;
   totalPages!: number;
 
-  searchProduct!:string;
+  searchProduct!: string;
   constructor(private productServ: productsService) {
     this.page = new BehaviorSubject<number>(this.currentPage);
   }
   // hook
   ngOnInit() {
-    this.page.subscribe((newPage)=> {
+    this.page.subscribe((newPage) => {
       this.productServ.getAllProducts(newPage).subscribe((response) => {
-      this.products=response.products;
+        this.products = response.products;
+      });
     });
-    })
-
   }
-  prevPage(){
-    if (this.currentPage > 1)
-      this.page.next(--this.currentPage);
+  prevPage() {
+    if (this.currentPage > 1) this.page.next(--this.currentPage);
   }
-  nextPage(){
-    if (this.products[this.products.length-1].id != 194)
+  nextPage() {
+    if (this.products[this.products.length - 1].id != 194)
       this.page.next(++this.currentPage);
   }
 
   searchForProducts() {
-    this.productServ.searchForProduct(this.searchProduct).subscribe((response) => {
-      this.products=response.products;
-    });
+    this.productServ
+      .searchForProduct(this.searchProduct)
+      .subscribe((response) => {
+        this.products = response.products;
+      });
   }
 }
