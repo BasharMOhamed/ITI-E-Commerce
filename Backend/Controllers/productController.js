a7724e772ebd670d95b13abf55227f9582fd2ab7
hussien103 <2000459@eng.asu.edu.eg>
2024-08-25 03:44:18 +0300

    added products backend

---
+++ Backend/Controllers/productController.js
@@ -0,0 +1,106 @@
+const Product = require('../Models/Product');
+
+const fs = require('fs');
+const path = require('path');
+/*
+async function insertDataFromJson() {
+    const dataFilePath = path.join(__dirname, '../Utils/data.json');
+    try {
+        const data = require(dataFilePath);
+        if (Array.isArray(data.products)) {
+            // Validate each product against the schema
+            const validProducts = data.products.filter(product => {
+                const isValid = product.id && 
+                                product.title && 
+                                product.description && 
+                                product.category && 
+                                product.price && 
+                                product.discountPercentage 
+                                
+
+                if (!isValid) {
+                    console.log(`Product with ID ${product.id} is invalid:`, product);
+                } else {
+                    console.log(`Valid product:`, product);
+                }
+                return isValid;
+            });
+
+            // Log the number of valid products
+            console.log(`Valid products count: ${validProducts.length}`);
+
+            await Product.insertMany(validProducts);
+            console.log('Data inserted successfully');
+        } else {
+            console.error('Data is not an array of products');
+        }
+    } catch (error) {
+        console.error('Error inserting data:', error);
+    }
+}
+*/
+async function getAllProducts() {
+
+    try {
+        const data = await Product.find({}); 
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error); 
+        throw error; 
+    }
+
+}
+
+async function getProductById(id) {
+    const data = await Product.findOne({ id: id });
+    return data;
+}
+
+async function addProduct(newProduct){
+    try {
+        const data = await Product.create(newProduct); 
+        console.log(data); 
+        return data; 
+    } catch (error) {
+        console.error(error); 
+        throw error; 
+    }
+}
+
+
+async function deleteProduct(id) {
+    try {
+        const data = await Product.findOneAndDelete({ id: id }); 
+        if (data) {
+            console.log(`Product with id ${id} deleted successfully`); 
+            return data; 
+        } else {
+            console.error(`Product with id ${id} not found`); 
+            throw new Error(`Product with id ${id} not found`); 
+        }
+    } catch (error) {
+        console.error(error); 
+        throw error; 
+    }
+}
+
+async function updateProduct(id, updates) {
+    try {
+        const data = await Product.findOneAndUpdate({ id: id }, updates, { new: true }); 
+        if (data) {
+            console.log(`Product with id ${id} updated successfully`); 
+            return data; 
+        } else {
+            console.error(`Product with id ${id} not found`); 
+            throw new Error(`Product with id ${id} not found`); 
+        }
+    } catch (error) {
+        console.error(error); 
+        throw error; 
+    }
+}
+
+
+
+module.exports = { getAllProducts, getProductById, addProduct,deleteProduct,updateProduct};
--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./Config/database");
 const userRoutes = require("./Routes/userRoutes.js");
+const productRoutes = require("./Routes/productRoutes.js"); 
+//const {insertDataFromJson} = require("./Controllers/productController.js");
 const app = express();
-const PORT = 4200;
+const PORT = 3200;
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 connectDB();
-
+//insertDataFromJson()
 app.use("/api/users", userRoutes);
+app.use("/api/product", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is Running on port: ${PORT}`);
